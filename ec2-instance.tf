2c45ab3abf72ff680dae8cf2bd78212ffdc0e629
Issam Bouaouda <issam.bouaouda@gmail.com>
2023-10-05 21:10:39 +0200

    update project

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -20,6 +20,7 @@ provider "aws" {
 resource "aws_instance" "ec2demo" {
   ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
+  user_data = file("${path.module}/app1-install.sh")
   key_name = var.key-value
 }
 
